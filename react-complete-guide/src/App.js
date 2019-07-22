b84a581115fb4ac5e3eaa92fc9ad12846d60994c
JonasLopesOSF <jonas.lopes@osf-global.com>
2019-07-22 18:56:02 -0300

    ..

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
+import Radium, { StyleRoot } from 'radium'
 
 class App extends Component {
   state = {
@@ -60,11 +61,16 @@ class App extends Component {
 
   render() {
     const styleButton = {
-      backgroundColor: 'white',
+      backgroundColor: 'green',
+      color: 'white',
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     }
 
     let persons = null;
@@ -82,24 +88,41 @@ class App extends Component {
           })}
         </div> 
       );
+
+      styleButton.backgroundColor = 'red';
+      styleButton[':hover'] = {
+        backgroundColor: 'salmon',
+        color: 'black'
+      };
+    }
+
+    const classes = [];
+    if (this.state.persons.length <= 2) {
+      classes.push('red'); // classes = ['red']
+    }
+    if (this.state.persons.length <= 1) {
+
+      classes.push('bold'); // classes = ['red', 'bold']
     }
 
     return (
+      <StyleRoot>
       <div className="App">
         <h1>Hi, I'm a React App</h1>
-        <p>This is really working!</p>
+        <p className={classes.join(' ')}>This is really working!</p>
         <button 
           style={styleButton}
           onClick={this.togglePersonsHandler}>Toggle Persons</button> 
           {/*it can be inefficient, us  e bind instead*/}
           {persons}
       </div>
+      </StyleRoot>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
-export default App;
+export default Radium(App);
 
 /*
   {
