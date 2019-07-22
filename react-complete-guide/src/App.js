531c05ae01670996d8cf593506948d8f20347992
JonasLopesOSF <jonas.lopes@osf-global.com>
2019-07-22 16:31:17 -0300

    conditional lists
    using map,
    variables in javascript to render elements in JSX
    conditional comparations tu show or not elementes in the page

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -5,33 +5,57 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Max', age: 28 },
-      { name: 'Manu', age: 29 },
-      { name: 'Stephanie', age: 26 }
+      { id: 'asdf', name: 'Max', age: 28 },
+      { id: 'sdgga', name: 'Manu', age: 29 },
+      { id: 'jjuik', name: 'Stephanie', age: 26 }
     ],
-    otherState: 'some other value'
+    otherState: 'some other value',
+    showPersons: false,
   };
 
-  switchNameHandler = (newName) => {
-    // console.log('Was clicked!');
-    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
-    this.setState({
-      persons: [
-        { name: newName, age: 28 },
-        { name: 'Manu', age: 29 },
-        { name: 'Stephanie', age: 27 }
-      ]
-    });
-  };
+  // switchNameHandler = (newName) => {
+  //   // console.log('Was clicked!');
+  //   // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+  //   this.setState({
+  //     persons: [
+  //       { name: newName, age: 28 },
+  //       { name: 'Manu', age: 29 },
+  //       { name: 'Stephanie', age: 27 }
+  //     ]
+  //   });
+  // };
 
-  nameChangedHandler = (event) => {
-    this.setState({
-      persons: [
-        { name: 'Max', age: 28 },
-        { name: event.target.value, age: 29 },
-        { name: 'Stephanie', age: 26 }
-      ]
+
+  nameChangedEventHandler = ( event, id ) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
     });
+
+    const person = {
+      ...this.state.persons[personIndex]
+    }
+    //const person = Object.assign({}, this.state.persons[personIndex]); -- alternative
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+    
+    this.setState({
+      persons: persons
+    });
+  }
+  
+  deletePersonHandler = (personIndex) => {
+    // const persons = this.state.persons.slice();
+    const persons = [...this.state.persons]; // beautifull way of make a copy of array
+    persons.splice(personIndex, 1); // remove the element passed like argument
+    this.setState({ persons: persons });
+  }
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({ showPersons: !doesShow });
   }
 
   render() {
@@ -43,29 +67,32 @@ class App extends Component {
       cursor: 'pointer'
     }
 
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return <Person 
+                      click={() => this.deletePersonHandler(index)}
+                      name={person.name} 
+                      age={person.age}
+                      key={person.id}
+                      changed={(event) => this.nameChangedEventHandler(event, person.id)} />
+          })}
+        </div> 
+      );
+    }
+
     return (
       <div className="App">
         <h1>Hi, I'm a React App</h1>
         <p>This is really working!</p>
         <button 
           style={styleButton}
-          onClick={() => this.switchNameHandler('Maximilian!!')}>Switch Name</button> {/*it can be inefficient, use bind instead*/}
-        <Person
-          name={this.state.persons[0].name}
-          age={this.state.persons[0].age}
-        />
-        <Person
-          name={this.state.persons[1].name}
-          age={this.state.persons[1].age}
-          click={this.switchNameHandler.bind(this, 'Max!')}
-          changed={this.nameChangedHandler}
-        >
-          My Hobbies: Racing
-        </Person>
-        <Person
-          name={this.state.persons[2].name}
-          age={this.state.persons[2].age}
-        />
+          onClick={this.togglePersonsHandler}>Toggle Persons</button> 
+          {/*it can be inefficient, us  e bind instead*/}
+          {persons}
       </div>
     );
     // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
@@ -73,3 +100,11 @@ class App extends Component {
 }
 
 export default App;
+
+/*
+  {
+    this.state.showPersons ?
+    </div> : null
+  }  
+      use this is one way to show in conditional html
+*/
