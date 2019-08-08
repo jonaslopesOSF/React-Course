2e55944b7b8cc33f2f613594c359dc33ff3c3c3c
JonasLopesOSF <jonas.lopes@osf-global.com>
2019-08-08 14:47:53 -0300

    structure to build burger app

--- buid-burger/src/App.js
+++ buid-burger/src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
+import Layout from './components/Layout/Layout';
+import BurgerBuilder from './container/BurgerBuilder/BurgerBuilder';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Layout>
+          <BurgerBuilder />
+        </Layout>
       </div>
     );
   }
