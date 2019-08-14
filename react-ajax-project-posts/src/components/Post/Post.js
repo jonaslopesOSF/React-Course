fd97949d246bad447cd12456640585683603b062
JonasLopesOSF <jonas.lopes@osf-global.com>
2019-08-14 17:19:21 -0300

    change project structure
    Creating Rotes

--- react-ajax-project-posts/src/components/Post/Post.js
+++ react-ajax-project-posts/src/components/Post/Post.js
@@ -1,14 +1,14 @@
-import React from 'react';
+import React from "react";
 
-import './Post.css';
+import "./Post.css";
 
-const post = ({title, author, clicked}) => (
+const post = ({ title, author, clicked }) => (
     <article className="Post" onClick={clicked}>
-        <h1>{title}</h1>
-        <div className="Info">
-            <div className="Author">{author}</div>
-        </div>
+      <h1>{title}</h1>
+      <div className="Info">
+        <div className="Author">{author}</div>
+      </div>
     </article>
-);
+  );
 
 export default post;
--- react-ajax-project-posts/src/containers/Blog/Blog.js
+++ react-ajax-project-posts/src/containers/Blog/Blog.js
@@ -1,66 +1,41 @@
 import React, { Component } from "react";
 // import axios from "axios";
-import axios from '../../axios';
-
-import Post from "../../components/Post/Post";
-import FullPost from "../../components/FullPost/FullPost";
-import NewPost from "../../components/NewPost/NewPost";
 import "./Blog.css";
+import { Route, NavLink } from 'react-router-dom';
+import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
+import FullPost from './FullPost/FullPost';
 
 class Blog extends Component {
-  state = {
-    posts: [],
-    selectedPostId: null,
-    error: false
-  };
-
-  componentDidMount() {
-    axios.get( "/posts" )
-      .then(response => {
-        const posts = response.data.slice(0, 4);
-        const updatedPosts = posts.map(post => {
-          return {
-            ...post,
-            author: 'Jonas',
-          }
-        })
-        this.setState({ posts: updatedPosts });
-        // console.log(response);
-      })
-  }
-
-  postSelectedHandler = (id) => {
-    this.setState({ selectedPostId: id })
-  }
-
-  getPosts = () => {
-    let posts = <p style={{textAlign: "center"}}>Something went wrong with load posts...</p>
-
-    if (!this.state.error) {
-      posts = this.state.posts.map(post => {
-        return <Post 
-          key={post.id}
-          title={post.title}
-          author={post.author}
-          clicked={() => this.postSelectedHandler(post.id)}
-          />
-      });  
-    }
-    return posts;
-  } 
-
-  render() {
+   render() {
     return (
-      <div>
-        <section className="Posts">
-          {this.getPosts()}
-        </section>
-        <section>
-          <FullPost id={this.state.selectedPostId}/>
-        </section>
-        <section>
-          <NewPost />
-        </section>
+      <div className="Blog">
+        <header>
+          <nav>
+            <ul>
+              <li><NavLink 
+                    to="/"
+                    exact 
+                    activeClassName="my-active"
+                    activeStyle={{
+                      color: '#fa923f',
+                      textDecoration: 'underline',
+                    }}>Home</NavLink></li>
+                    {/* activeClassName is your custom className
+                     to the active class */}
+              <li><NavLink to={{
+                pathname: "/new-post",
+                hash: "#submit",
+                search: '?quick-submit=true'
+              }}>New Post</NavLink></li>
+            </ul>
+          </nav>
+        </header>
+        {/* <Route exact path="/" render={() =>  <h1>Home</h1>} />
+        <Route path="/" render={() =>  <h1>Home</h1>} /> */}
+        <Route exact path="/" component={Posts} />
+        <Route path="/new-post" component={NewPost} />
+        <Route path="/:id" component={FullPost} />
       </div>
     );
   }
