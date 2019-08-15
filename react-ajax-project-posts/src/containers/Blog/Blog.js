70f4b5f149ad49cc0147668d61b081ada78261b6
JonasLopesOSF <jonas.lopes@osf-global.com>
2019-08-15 13:46:14 -0300

    finished project routing

--- react-ajax-project-posts/src/containers/Blog/Blog.js
+++ react-ajax-project-posts/src/containers/Blog/Blog.js
@@ -1,43 +1,48 @@
 import React, { Component } from "react";
 // import axios from "axios";
 import "./Blog.css";
-import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
-import Posts from './Posts/Posts';
-import asyncComponent from '../../hoc/asyncComponent';
+import { Route, NavLink, Switch, Redirect } from "react-router-dom";
+import Posts from "./Posts/Posts";
+import asyncComponent from "../../hoc/asyncComponent";
 // import NewPost from './NewPost/NewPost';
-const AsyncNewPost =  asyncComponent(() => {
-  return import('./NewPost/NewPost');
+const AsyncNewPost = asyncComponent(() => {
+  return import("./NewPost/NewPost");
 });
 
 class Blog extends Component {
   state = {
     auth: true
-  }
+  };
 
-   render() {
+  render() {
     return (
       <div className="Blog">
         <header>
           <nav>
             <ul>
-              <li><NavLink 
-                    to="/posts/"
-                    exact 
-                    activeClassName="my-active"
-                    activeStyle={{
-                      color: '#fa923f',
-                      textDecoration: 'underline',
-                    }}>
-                      Posts
-                    </NavLink></li>
-                    {/* activeClassName is your custom className
+              <li>
+                <NavLink
+                  to="/posts/"
+                  exact
+                  activeClassName="my-active"
+                  activeStyle={{
+                    color: "#fa923f",
+                    textDecoration: "underline"
+                  }}
+                >
+                  Posts
+                </NavLink>
+              </li>
+              {/* activeClassName is your custom className
                      to the active class */}
               <li>
-                <NavLink to={{
-                  pathname: "/new-post",
-                  hash: "#submit",
-                  search: '?quick-submit=true'
-              }}>
+                <NavLink
+                  to={{
+                    pathname: "/new-post",
+                    hash: "#submit",
+                    search: "?quick-submit=true"
+                  }}
+                >
                   New Post
                 </NavLink>
               </li>
@@ -47,7 +52,9 @@ class Blog extends Component {
         {/* <Route exact path="/" render={() =>  <h1>Home</h1>} />
         <Route path="/" render={() =>  <h1>Home</h1>} /> */}
         <Switch>
-          {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null}
+          {this.state.auth ? (
+            <Route path="/new-post" component={AsyncNewPost} />
+          ) : null}
           <Route path="/posts" component={Posts} />
           <Route render={() => <h1>Not found</h1>} />
           {/* <Redirect from="/" to="/posts" /> */}
--- routing--assignment-problem/src/App.js
+++ routing--assignment-problem/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import Courses from './containers/Courses/Courses';
+import Course from './containers/Course/Course';
 import Users from './containers/Users/Users';
+import NoMatch from './components/NoMatch/NoMatch';
 
 class App extends Component {
   render () {
     return (
       <div className="App">
-        <ol style={{textAlign: 'left'}}>
+        <ol style={{ textAlign: 'left' }}>
           <li>Add Routes to load "Users" and "Courses" on different pages (by entering a URL, without Links)</li>
           <li>Add a simple navigation with two links => One leading to "Users", one leading to "Courses"</li>
           <li>Make the courses in "Courses" clickable by adding a link and load the "Course" component in the place of "Courses" (without passing any data for now)</li>
@@ -17,6 +20,23 @@ class App extends Component {
           <li>Add a 404 error page and render it for any unknown routes</li>
           <li>Redirect requests to /all-courses to /courses (=> Your "Courses" page)</li>
         </ol>
+        <nav>
+          <ul style={{ listStyle: 'none', margin: 'auto', padding: '0' }}>
+            <li style={{ margin: '10px', display: 'inline-block' }}>
+              <NavLink to="/courses">Courses</NavLink>
+            </li>
+            <li style={{ margin: '10px', display: 'inline-block' }}>
+              <NavLink to="/users">Users</NavLink>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/users" component={Users} />
+          {/* <Route path="/courses/:courseId" component={Course} /> */}
+          <Route path="/courses" component={Courses} />
+          <Redirect from="/all-courses" to="/courses" />
+          <Route component={NoMatch}/>
+        </Switch>
       </div>
     );
   }
