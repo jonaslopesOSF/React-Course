015204299fa42ed702a6893af7837ca6ad9aaf48
JonasLopesOSF <jonas.lopes@osf-global.com>
2019-09-17 15:28:12 -0300

    redux basics, learning

---
+++ redux/src/store/reducer.js
@@ -0,0 +1,44 @@
+import * as actionTypes from './actions';
+
+const initialState = {
+  counter: 0,
+  results: [],
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case actionTypes.INCREMENT:
+      const newState = Object.assign({}, state);
+      newState.counter = state.counter + 1;
+      return newState
+    case actionTypes.DECREMENT:
+      return {
+        ...state,
+        counter: state.counter - 1
+      };
+    case actionTypes.ADD:
+      return {
+        ...state,
+        counter: state.counter + action.value
+      };
+    case actionTypes.SUBTRACT:
+      return {
+        ...state,
+        counter: state.counter - action.value
+      };
+    case actionTypes.STORE_RESULT:
+      return {
+        ...state,
+        results: state.results.concat({id: new Date(), value: state.counter}),
+      };
+    case actionTypes.DELETE_RESULT:
+      const updatedArray = state.results.filter(result => result.id !== action.resultElId);
+      return {
+        ...state,
+        results: updatedArray
+      };
+  }
+  return state;
+};
+
+export default reducer;
