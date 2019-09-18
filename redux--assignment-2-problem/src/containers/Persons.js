b3ba11f6c4aaed974f8b00df0118906fc8cf3b1a
JonasLopesOSF <jonas.lopes@osf-global.com>
2019-09-18 15:55:42 -0300

    addind a little form

--- redux--assignment-2-problem/src/containers/Persons.js
+++ redux--assignment-2-problem/src/containers/Persons.js
@@ -1,24 +1,22 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 
 import Person from '../components/Person/Person';
 import AddPerson from '../components/AddPerson/AddPerson';
-
-import { connect } from 'react-redux';
 import * as actionTypes from '../store/actions';
 
-
 class Persons extends Component {
-
+    
     render () {
         return (
             <div>
-                <AddPerson personAdded={this.props.onAddPerson} />
-                {this.props.persons.map(person => (
-                    <Person
+                <AddPerson personAdded={this.props.onAddedPerson} />
+                {this.props.prs.map(person => (
+                    <Person 
                         key={person.id}
-                        name={person.name}
-                        age={person.age}
-                        clicked={() => this.props.onDeletePerson(person.id)}/>
+                        name={person.name} 
+                        age={person.age} 
+                        clicked={() => this.props.onRemovedPerson(person.id)}/>
                 ))}
             </div>
         );
@@ -26,16 +24,16 @@ class Persons extends Component {
 }
 
 const mapStateToProps = state => {
-  return {
-    persons: state.persons
-  }
-}
+    return {
+        prs: state.persons
+    };
+};
 
-const mapDispatcherToProps = dispatch => {
-  return {
-    onAddPerson: () => dispatch({type: actionTypes.ADD_PERSON}),
-    onDeletePerson: (id) => dispatch({type: actionTypes.DELETE_PERSON, personId: id})
-  }
-}
+const mapDispatchToProps = dispatch => {
+    return {
+        onAddedPerson: (name, age) => dispatch({type: actionTypes.ADD_PERSON, personData: {name: name, age: age}}),
+        onRemovedPerson: (id) => dispatch({type: actionTypes.REMOVE_PERSON, personId: id})
+    }
+};
 
-export default connect(mapStateToProps, mapDispatcherToProps)(Persons);
+export default connect(mapStateToProps, mapDispatchToProps)(Persons);
